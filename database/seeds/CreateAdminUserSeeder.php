4ddb3d3492d89b9e78ea85d2f9aebbc03b1e7a2e
anwarshah <syed.anwar@nextgeni.dev>
2020-07-10 17:17:04 +0500

    LARAVEL ACL ACHIEVED

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\User;
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = User::create([
+        	'name' => 'Syed Anwar Ahmed shah', 
+        	'email' => 'shah@gmail.com',
+        	'password' => Hash::make('shahji444')
+        ]);
+  
+        $role = Role::create(['name' => 'Admin']);
+   
+        $permissions = Permission::pluck('id','id')->all();
+  
+        $role->syncPermissions($permissions);
+   
+        $user->assignRole([$role->id]);
+    }
+}
