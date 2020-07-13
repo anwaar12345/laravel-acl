e4a35203ea06affd57f0d69108f5f2bc11c7ac35
anwarshah <syed.anwar@nextgeni.dev>
2020-07-13 10:35:56 +0500

    added correct middleware

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -20,11 +20,13 @@ class RoleController extends Controller
      */
     function __construct()
     {
-        $this->middleware('role:admin');
-        //  $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
-        //  $this->middleware('permission:role-create', ['only' => ['create','store']]);
-        //  $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
-        //  $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+        $this->middleware('role:Admin');
+    
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:role-create', ['only' => ['create','store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+
     }
 
 
